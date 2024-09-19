9fce29979e4837e72d267133cc1edb1c8f0cacd9
you-mo77 <kano1201971@gmail.com>
2024-09-19 19:12:09 +0900

    なんか色付けが安定しない クラスタリングはできてる

--- class.py
+++ class.py
@@ -11,9 +11,12 @@ import bar_plot
 import tempfile
 
 # gui(どの主成分を使うかを2つのプルダウンメニューとかで指定　クラスタ数も指定 -> GO!)
-def gui(score:list[str]):
+def gui(score:pd.DataFrame, df:pd.DataFrame):
+    #コンボボックス用リスト
+    pc_name = score.columns.tolist()
+
     # ウィンドウレイアウト
-    layout = [[sg.Text("横軸"), sg.Combo(score, key="-x-"), sg.Text("   "), sg.Text("縦軸"), sg.Combo(score, key="-y-")],
+    layout = [[sg.Text("横軸"), sg.Combo(pc_name, key="-x-"), sg.Text("   "), sg.Text("縦軸"), sg.Combo(pc_name, key="-y-"), sg.Text("   "), sg.Text("クラスタ数"), sg.Input(key="-cluster-")],
               [sg.Image(filename="", key="-image-")],
               [sg.Button("Run")]]
     
@@ -28,18 +31,26 @@ def gui(score:list[str]):
         if event == sg.WIN_CLOSED:
             break
 
+        # 画像更新
+        if event == "Run":
+            # 画像作成
+            k_means(df, score, values["-x-"], values["-y-"], int(values["-cluster-"]))
+
 
 
 
 # K-means 
 def k_means(df:pd.DataFrame, score:pd.DataFrame, pc1:str, pc2:str, n:int):
+    print(f" *****pc1:{pc1} pc2:{pc2} cluster:{n}***** ")
+    
     # 街路番号抽出してscoreのインデックスへ
     load_num = df.index
-    #load_num = load_num.tolist()
+
+    # load_num = load_num.tolist()
     score.index = load_num
 
     # Kmeansインスタンス作成
-    kmeans = KMeans(n_clusters=n, max_iter=30)
+    kmeans = KMeans(n_clusters=n, max_iter=300)
     
     # モデルを学習 -> クラスタリング
     class_label = kmeans.fit_predict(score[[pc1, pc2]])
@@ -47,7 +58,7 @@ def k_means(df:pd.DataFrame, score:pd.DataFrame, pc1:str, pc2:str, n:int):
     # クラスcolumns作成
     score["class"] = class_label
 
-    #print(score)
+    print(score["class"])
 
     # グラフ作成
     plt.xlabel(pc1)
@@ -73,7 +84,7 @@ def main():
     n = 8
     (score, eigenvalue, ratio, eigen, v_name) = bar_plot.pca(df, n)
 
-    gui(score.columns.tolist())
+    gui(score, df)
 
     
 
